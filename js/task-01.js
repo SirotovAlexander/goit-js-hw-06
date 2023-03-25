02fc72b13b05aa3944766ed26c14efd2f9969af8
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-26 00:04:31 +0200

    xueta

--- js/task-01.js
+++ js/task-01.js
@@ -5,3 +5,18 @@ console.dir(`Number of categories: ${categories.children.length}`);
   console.log(`Category: ${element.querySelector("h2").textContent}`);
   console.log(`Elements: ${element.querySelectorAll("li").length}`);
 });
+
+// const categories = document.querySelector("#categories");
+// console.dir(`Number of categories: ${categories.children.length}`);
+
+// [...categories.children].forEach((element) => {
+//   console.log(`Category: ${element.querySelector("h2").textContent}`);
+//   console.log(`Elements: ${element.querySelectorAll("li").length}`);
+// });
+// const categories = document.querySelector("#categories");
+// console.dir(`Number of categories: ${categories.children.length}`);
+
+// [...categories.children].forEach((element) => {
+//   console.log(`Category: ${element.querySelector("h2").textContent}`);
+//   console.log(`Elements: ${element.querySelectorAll("li").length}`);
+// });
