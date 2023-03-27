1ccdf2f14e25997b2eab558c68037420ee06a64a
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-27 22:34:14 +0300

    done 10
    done 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxControl = document.querySelector("#controls");
+
+boxControl.children[0].addEventListener("input", createBoxes);
+boxControl.children[1].addEventListener("click", pushBoxes);
+boxControl.children[2].addEventListener("click", destroyBoxes);
+const newBox = document.querySelector("#boxes");
+
+function createBoxes(event) {
+  event.currentTarget.valueAsNumber;
+}
+
+function pushBoxes(event) {
+  const newDi = document.createElement("div");
+  newDi.style.backgroundColor = `${getRandomHexColor()}`;
+  newDi.style.width = `30px`;
+  newDi.style.height = `30px`;
+  newDi.style.marginBottom = "12px";
+  newDi.style.marginTop = "12px";
+  newBox.prepend(newDi) * `${createBoxes(event)}`;
+}
+
+function destroyBoxes(event) {
+  newBox.innerHTML = "";
+}
