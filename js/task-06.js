b80b01cd5c4da3ec05a29f9d2953adc3d19a221a
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-27 16:23:12 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,7 @@ const inputText = document.querySelector("#validation-input");
 const lengthCounter = Number(inputText.dataset.length);
 
 
-// inputText.addEventListener("input", onInput);
 
-// function onInput (event) {
-//     console.dir(event.currentTarget)
-// }
 
 inputText.addEventListener("blur", onBlur);
 
@@ -15,8 +11,8 @@ function onBlur (event) {
         if(event.currentTarget.value.length >= 1 && event.currentTarget.value.length < lengthCounter) {
             return inputText.classList.add("invalid")
         } else if (event.currentTarget.value.length >= lengthCounter) {
+             inputText.classList.remove("invalid");
             return inputText.classList.add("valid");
         }
-return inputText.classList.remove("invalid") || inputText.classList.remove("valid");
-
+    return inputText.classList.remove("invalid") || inputText.classList.remove("valid");
 }
