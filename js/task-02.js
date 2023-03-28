a00fe70a233a30ddcceb8ac168882d98c07c4528
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-28 23:40:51 +0300

    fixed-mist

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,12 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
+let newIngredientArr = [];
+
 for (let i = 0; i < ingredients.length; i += 1) {
   const newIngredient = document.createElement("li");
   newIngredient.textContent = `${ingredients[i]}`;
   newIngredient.classList.add("item");
-  ingredientsList.append(newIngredient);
+  newIngredientArr.push(newIngredient);
 }
+ingredientsList.append(...newIngredientArr);
