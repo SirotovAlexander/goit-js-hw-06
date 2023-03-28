42b1cbbe2bd9d638e74bdab12fdebe225d6cf382
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-29 00:23:58 +0300

    fixed-mist

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,32 @@
-const form = document.querySelector(".login-form")
+const form = document.querySelector(".login-form");
 form.addEventListener("submit", handleSubmit);
-console.dir(form.elements)
-function handleSubmit(event) {
-    event.preventDefault();
-    const {
-      elements: { email, password }
-    } = event.currentTarget;
-  
 
-    if (email.value === "" || password.value === "") {
-      return alert("Заповніть всі поля!");
-    }
-  
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
-    event.currentTarget.reset();
+function handleSubmit(event) {
+  const personInfo = {};
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заповніть всі поля!");
   }
+  personInfo.email = `${email.value}`;
+  personInfo.password = `${password.value}`;
+  console.log(personInfo);
+  event.currentTarget.reset();
+}
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const {
+//       elements: { email, password }
+//     } = event.currentTarget;
+
+//     if (email.value === "" || password.value === "") {
+//       return alert("Заповніть всі поля!");
+//     }
+
+//     console.log(`Email: ${email.value}, Password: ${password.value}`);
+//     event.currentTarget.reset();
+//   }
