1edf043007eed1072c6e172bcc3705c6b18ddfd0
SirotovAlexander <alexander.sirotov@gmail.com>
2023-03-29 00:18:42 +0300

    fixed-mist

--- js/task-06.js
+++ js/task-06.js
@@ -4,16 +4,18 @@ const lengthCounter = Number(inputText.dataset.length);
 inputText.addEventListener("blur", onBlur);
 
 function onBlur(event) {
-  if (
-    event.currentTarget.value.length >= 1 &&
-    event.currentTarget.value.length < lengthCounter
-  ) {
+  if (event.currentTarget.value.length === 0) {
+    return (
+      inputText.classList.remove("invalid") ||
+      inputText.classList.remove("valid")
+    );
+  } else if (event.currentTarget.value.length < lengthCounter) {
     return inputText.classList.add("invalid");
-  } else if (event.currentTarget.value.length >= lengthCounter) {
+  } else if (event.currentTarget.value.length === lengthCounter) {
     inputText.classList.remove("invalid");
     return inputText.classList.add("valid");
+  } else if (event.currentTarget.value.length > lengthCounter) {
+    inputText.classList.remove("valid");
+    return inputText.classList.add("invalid");
   }
-  return (
-    inputText.classList.remove("invalid") || inputText.classList.remove("valid")
-  );
 }
